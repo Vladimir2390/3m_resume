70c151b23ec64e12bb8acce9e7cdba0900830d3f
Vladimir <air90@bk.ru>
2022-09-21 17:04:23 +0500

    Реализован алгоритм

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] Array = { "-)", "Rows", "MEGA!", "Mobilization", "52", "235" };
+string opa = string.Join(", ", Array);
+Console.Write($"[{opa}] -> ");
+for (int i = 0; i < Array.Length; i++)
+{
+    if (Array[i].Length <= 3)
+    {
+        if (i != Array.Length - 1)
+        {
+            Console.Write($"{Array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{Array[i]}");
+        }
+    }
+}
